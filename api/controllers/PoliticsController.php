e6f6d72b1a5507702df5e72e6c7f910e65eb2cbb
Temirbek Nuradil <temirbek@gmail.com>
2017-11-16 18:23:59 +0600

    politics modification2

--- api/controllers/PoliticsController.php
+++ api/controllers/PoliticsController.php
@@ -32,12 +32,15 @@ class PoliticsController extends \yii\rest\ActiveController
         }
         // prepare and return a data provider for the "index" action
         $request=\Yii::$app->request->get();
-        $text='';
+        $ctg='';$text='';
+        if(isset($request['category_id'])){
+            $ctg=$request['category_id'];
+        }
         if(isset($request['text'])){
             $text=$request['text'];
         }
         $query =Politics::find();
-
+        $query->filterWhere(['category_id'=>$ctg]);
         $query->andFilterWhere(['or',['like','title',$text],['like','text',$text]]);
 
 
