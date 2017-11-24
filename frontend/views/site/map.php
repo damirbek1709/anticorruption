530a257913a39e62ff5e68ca80500b15ca6aab91
Damirbek <damirbek@gmail.com>
2017-11-24 09:32:02 +0600

    map drop value

--- frontend/views/site/map.php
+++ frontend/views/site/map.php
@@ -1,6 +1,8 @@
 <?php
+
 use frontend\models\Report;
 use yii\helpers\Html;
+
 ?>
 <div class="main_heading">
     <?= Yii::t('app', 'Карта коррупции'); ?>
@@ -8,25 +10,31 @@ use yii\helpers\Html;
 <div class="sort-wrap">
     <div class="sort-label">Сортировать по:</div>
     <?= Html::beginForm(['site/map'], 'get', ['id' => 'map-filter']); ?>
-    <?= Html::dropDownList('authority', null, Report::getAuthorities(), ['onchange'=>'this.form.submit()','prompt' => 'госоргану', 'class' => 'sort-select authority-select']) ?>
-    <?= Html::dropDownList('sector',
-        null, Report::getSingleDrop('report_category'),
+    <?= Html::dropDownList('authority', $authority, Report::getAuthorities(),
         [
-            'onchange'=>'this.form.submit()',
+            'onchange' => 'this.form.submit()',
+            'prompt' => 'госоргану',
+            'class' => 'sort-select authority-select'
+        ]); ?>
+    <?= Html::dropDownList('sector',
+        $sector, Report::getSingleDrop('report_category'),
+        [
+            'onchange' => 'this.form.submit()',
             'prompt' => 'сектору коррупции',
             'class' => 'sort-select sector-select'
         ]); ?>
     <?= Html::dropDownList('city',
-        null, Report::getDropdownItems('city'),
+        $city, Report::getDropdownItems('city'),
         [
+            'onchange' => 'this.form.submit()',
             'prompt' => 'местоположению',
             'class' => 'sort-select city-select'
         ]); ?>
 
     <?= Html::dropDownList('type',
-        null, Report::getSingleDrop('report_type'),
+        $type, Report::getSingleDrop('report_type'),
         [
-            'onchange'=>'this.form.submit()',
+            'onchange' => 'this.form.submit()',
             'prompt' => 'типу обращения',
             'class' => 'sort-select type-select'
         ]); ?>
@@ -36,5 +44,6 @@ use yii\helpers\Html;
 </div>
 
 
-<div id="map_long" authority="<?=$authority?>" sector="<?=$sector?>" city="<?=$city?>" type="<?=$type?>" class="map" style="height: 550px;"></div>
+<div id="map_long" authority="<?= $authority ?>" sector="<?= $sector ?>" city="<?= $city ?>" type="<?= $type ?>"
+     class="map" style="height: 550px;"></div>
 
