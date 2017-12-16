cc5c9c79b8c09aa21e1a7295133affe69304eb13
Damirbek <damirbek@gmail.com>
2017-12-16 14:56:26 +0600

    start check

--- frontend/views/qwert/_form.php
+++ frontend/views/qwert/_form.php
@@ -2,7 +2,10 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\file\FileInput;
+use yii\helpers\ArrayHelper;
+use frontend\models\Vocabulary;
+use kartik\datetime\DateTimePicker;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Qwert */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,7 +15,74 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true,
+        'placeholder' => 'Введите заголовок вашего сообщения',
+        'class' => 'form-control sharper'])->label(false); ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6, 'placeholder' => 'Расскажите подробнее о факте коррупции с которым вы столкнулись', 'class' => 'form-control comment-input-text'])->label(false); ?>
+
+    <?php
+    echo $form->field($model, 'category_id')
+        ->dropDownList(ArrayHelper::map(Vocabulary::find()->asArray()->where(['key' => 'report_category'])->all(), 'id', 'value'),
+            [
+                'prompt' => 'Выберите сектор коррупции',
+                'class' => 'form-control custom-drop'
+            ]
+        )->label(false);
+    ?>
+
+    <?php
+    echo $form->field($model, 'authority_id')->dropDownList(\frontend\models\Report::getAuthorities(), [
+        'prompt' => 'Выберите госорган или структуру',
+        'class' => 'form-control custom-drop'
+    ])->label(false); ?>
+
+    <?php
+    $type = 134;
+    if (isset($_POST['paramType'])) {
+        $type = $_POST['paramType'];
+    }
+    echo $form->field($model, 'type_id')
+        ->dropDownList(ArrayHelper::map(Vocabulary::find()->asArray()->where(['key' => 'report_type'])->all(), 'id', 'value'),
+            [
+                'value' => $type,
+                'prompt' => 'Выберите тип обращения',
+                'class' => 'form-control custom-drop'
+            ]
+        )->label(false);
+    ?>
+
+    <div class="form-group">
+        <? echo '<label>Дата и время</label>';
+        echo DateTimePicker::widget([
+            'model' => $model,
+            'name' => 'date',
+            'attribute' => 'date',
+            'options' => ['placeholder' => 'Выберите время дату и время'],
+            //'convertFormat' => true,
+            'pluginOptions' => [
+                //'minView' => 0,
+                'minuteStep' => 30,
+                //'format' => 'd-M-Y g:i A',
+                'format' => 'yyyy-m-dd HH:ii',
+                //'startDate' => '01-Mar-2017 12:00 AM',
+                'todayHighlight' => true
+            ]
+        ]); ?>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <?= $form->field($model, 'date')->textInput() ?>
 
